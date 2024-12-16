9dfcd174679a7766beb4687cfe0d90b7f5f7a6f5
Maksim Sukhov <silveraven@yandex.ru>
2024-12-16 17:44:06 +0200

    refactor(basic-3.3): add names of identifiers and classes concise

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="task-edit";
 
-    deleteButton.className="task-delete";
+    deleteButton.className="task-del";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -157,7 +157,7 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
     var editButton=taskListItem.querySelector("button.task-edit");
-    var deleteButton=taskListItem.querySelector("button.task-delete");
+    var deleteButton=taskListItem.querySelector("button.task-del");
 
 
     //Bind editTask to edit button.
